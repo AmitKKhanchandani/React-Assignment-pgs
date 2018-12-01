4123a0ffa903e29c2d8687d2f025b4b8aacd9c1a
Amit K <amitsystem@live.com>
2018-12-01 22:26:35 +0530

    Updates

---
+++ precache-manifest.c44d38c16242edb77539500d7f25c736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccbb74acd1a0d6b4b8ea",
+    "url": "/React-Assignment-pgs/static/js/runtime~main.ccbb74ac.js"
+  },
+  {
+    "revision": "2fdba6f55dcd4a5071c3",
+    "url": "/React-Assignment-pgs/static/js/main.2fdba6f5.chunk.js"
+  },
+  {
+    "revision": "bc421bc96425c8e13d23",
+    "url": "/React-Assignment-pgs/static/js/1.bc421bc9.chunk.js"
+  },
+  {
+    "revision": "2fdba6f55dcd4a5071c3",
+    "url": "/React-Assignment-pgs/static/css/main.52060c14.chunk.css"
+  },
+  {
+    "revision": "88b93b1dc509909cbdd004fd190b4e2f",
+    "url": "/React-Assignment-pgs/index.html"
+  }
+];
